6281a7dfc9e7113da989e07c4cc464282ca0fc32
Creatimatix <creatimatix@gmail.com>
2024-07-13 13:31:54 +0530

    center alignment for Products

--- Product.js
+++ Product.js
@@ -81,14 +81,14 @@ function AddProductstoArray(Product_Name, Product_LineName, Product_Image_URL, F
 			for(let i=start; i<end;i++){
 			
 				document.getElementById("products1").innerHTML += "<li class='product "+CurrentList[i].LineName+" "+CurrentList[i].Name+" item-gallery vertical-item' name='"+CurrentList[i].Name+"'>"
-				+"<div class='product-inner'>"
+				+"<div class='product-inner'> <center>"
 				+"<a class='woocommerce-LoopProduct-link woocommerce-loop-product__link' href='shop-product-left.html'>"
 				+"<img src='"+CurrentList[i].Image_URL+"' alt='Product_Image' style='max-height:150px'>"
 				+"</a>"	
-				+"<h2 class='woocommerce-loop-product__title'><a class='woocommerce-LoopProduct-link woocommerce-loop-product__link' href='shop-product-right.html'>"+CurrentList[i].Name+"</a></h2>"
+				+"<h2 class='woocommerce-loop-product__title'><center><a class='woocommerce-LoopProduct-link woocommerce-loop-product__link' href='shop-product-right.html'>"+CurrentList[i].Name+"</a></center></h2>"
 				+"<span class='price'>"
 				+"<ins><span><span><a href="+CurrentList[i].FilePath_URL+">View More Details</a></span></ins></span>"
-				+"</div>"
+				+"</center></div>"
 				+"</li>"
 			}
 
