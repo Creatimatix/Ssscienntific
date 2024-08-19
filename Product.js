9e5e9fcfa732952a7c4448aecc449fdd40f40410
Creatimatix <creatimatix@gmail.com>
2024-08-19 09:27:22 +0530

    fixed link issue for null product link.

--- Product.js
+++ Product.js
@@ -84,12 +84,12 @@ function AddProductstoArray(Product_Name, Product_LineName, Product_Image_URL, F
 			
 				document.getElementById("products1").innerHTML += "<li class='product "+CurrentList[i].LineName+" "+CurrentList[i].Name+" item-gallery vertical-item' name='"+CurrentList[i].Name+"'>"
 				+"<div class='product-inner'> <center>"
-				+"<a class='woocommerce-LoopProduct-link woocommerce-loop-product__link' href='shop-product-left.html'>"
+				+"<a class='woocommerce-LoopProduct-link woocommerce-loop-product__link' href="+CurrentList[i].FilePath_URL+" target='_blank'>"
 				+"<img src='"+CurrentList[i].Image_URL+"' alt='Product_Image' style='max-height:150px'>"
 				+"</br></br></a>"	
 				+"<h2 class='woocommerce-loop-product__title'><center><a class='woocommerce-LoopProduct-link woocommerce-loop-product__link' href='shop-product-right.html'>"+CurrentList[i].Name+"</a></center></h2>"
 				+"<span class='price'>"
-				+"</br>	<ins><span><span><a href="+CurrentList[i].FilePath_URL+" target='_blank'>View More Details</a></span></ins></span>"
+				+"</br><ins><span><span><a href="+CurrentList[i].FilePath_URL+" target='_blank'>View More Details</a></span></ins></span>"
 				+"</center></div>"
 				+"</li>"
 			}
